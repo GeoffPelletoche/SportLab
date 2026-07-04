1a06bb48b183c08985b83f0acd86b0213028dd94
GeoffPelletoche <geoffrey.pelletier@mailo.com>
2026-07-04 15:54:17 +0200

    Refactor loadFrenchFlairData to use new competition mapping

--- modules/frenchflair.js
+++ modules/frenchflair.js
@@ -1,14 +1,26 @@
-import { CONFIG } from "../core/config.js";
+import { analyzeMatch } from "../core/ai.js";
 import { getRugbyFixtures } from "../core/api.js";
 
 export async function loadFrenchFlairData() {
-  const comps = CONFIG.rugbyCompetitions;
 
-  const data = {};
+  const comps = {
+    top14: "top_14",
+    prod2: "pro_d2",
+    superRugby: "super_rugby_pacific",
+    npc: "bunnings_npc",
+    championsCup: "champions_cup",
+    internationalXV: "international_xv"
+  };
+
+  const results = {};
 
   for (const key in comps) {
-    data[key] = await getRugbyFixtures(comps[key]);
+    const data = await getRugbyFixtures(comps[key]);
+
+    results[key] = (data.response || []).slice(0, 5).map(match => {
+      return analyzeMatch(match);
+    });
   }
 
-  return data;
+  return results;
 }
