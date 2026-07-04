a2a4393bc1d45a60566b6706860e99ae20135b9a
GeoffPelletoche <geoffrey.pelletier@mailo.com>
2026-07-04 15:45:29 +0200

    Add configuration for sports API and competitions

---
+++ core/config.js
@@ -0,0 +1,21 @@
+export const CONFIG = {
+  apiKey: "YOUR_API_SPORTS_KEY",
+  baseUrl: "https://v3.football.api-sports.io",
+
+  footballLeagues: {
+    ligue1: 61,
+    laLiga: 140,
+    bundesliga: 78,
+    premierLeague: 39,
+    serieA: 135
+  },
+
+  rugbyCompetitions: {
+    top14: "top_14",
+    prod2: "pro_d2",
+    superRugby: "super_rugby_pacific",
+    npc: "bunnings_npc",
+    championsCup: "champions_cup",
+    internationalXV: "international_xv"
+  }
+};
