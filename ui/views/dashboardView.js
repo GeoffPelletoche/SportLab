895dd6dcbb7ff7e522912c55a3dbb909561b8798
GeoffPelletoche <geoffrey.pelletier@mailo.com>
2026-07-20 08:37:49 +0200

    Update dashboardView.js

--- ui/views/dashboardView.js
+++ ui/views/dashboardView.js
@@ -1,1 +1,70 @@
+/**
+ * SPORTLAB V3 — DASHBOARD VIEW
+ * Rôle :
+ * Afficher les différentes pages de SportLab.
+ */
 
+export function renderDashboard({
+  activePage = "home",
+  navigationHtml = "",
+  drawhunterHtml = "",
+  frenchflairHtml = "",
+  journalHtml = "",
+  portfolioHtml = "",
+  betsHtml = ""
+  
+}) {
+
+  let content = "";
+
+  switch (activePage) {
+
+    case "journal":
+      content = `
+        <section class="card">
+          ${journalHtml}
+        </section>
+      `;
+      break;
+
+    case "bets":
+      content = `
+    <section class="card">
+      ${betsHtml}
+    </section>
+  `;
+  break;
+
+    case "portfolio":
+      content = `
+        <section class="card">
+          <h2>💼 Portfolio</h2>
+          ${portfolioHtml}
+        </section>
+      `;
+      break;
+
+    case "home":
+    default:
+      content = `
+        <section class="card">
+          <h2>⚽ DrawHunter</h2>
+          ${drawhunterHtml}
+        </section>
+
+        <section class="card">
+          <h2>🏉 FrenchFlair</h2>
+          ${frenchflairHtml}
+        </section>
+      `;
+      break;
+  }
+
+  return `
+    <h1>🏟️ SportLab</h1>
+
+    ${navigationHtml}
+
+    ${content}
+  `;
+}
