b2e29be60e1f6b981b14d9e78a3e85c60e469feb
GeoffPelletoche <geoffrey.pelletier@mailo.com>
2026-07-20 09:04:13 +0200

    Update betSettlementEngine.js

--- core/engines/betSettlementEngine.js
+++ core/engines/betSettlementEngine.js
@@ -1,7 +1,7 @@
 import {
     getBets,
     updateBetSettlement
-} from "./core/stores/betsStore.js";
+} from "Sportlab/core/stores/betsStore.js";
 
 /*
  * Remplace cette URL uniquement si l'adresse de ton Worker est différente.
