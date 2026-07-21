5a6d854ae217d13a127a457b98e1e188fce53ee3
GeoffPelletoche <geoffrey.pelletier@mailo.com>
2026-07-21 11:55:01 +0200

    Update appService.js

--- services/appService.js
+++ services/appService.js
@@ -3,21 +3,25 @@
 import { loadDrawHunterMatches } from "../modules/drawhunter.js";
 import { loadFrenchFlairMatches } from "../modules/frenchflair.js";
 
-import { getPortfolioSummary } from "./portfolioService.js";
-import { getAllBets } from "./betService.js";
+import { getDashboardData } from "./dashboardService.js";
 
 import { getAnalyses } from "../core/stores/analysisStore.js";
 
 export async function loadApplicationData() {
-  const drawhunterPayload = await loadDrawHunterMatches();
-  const frenchflairPayload = await loadFrenchFlairMatches();
+  const [
+    drawhunterPayload,
+    frenchflairPayload
+  ] = await Promise.all([
+    loadDrawHunterMatches(),
+    loadFrenchFlairMatches()
+  ]);
 
   return {
     drawhunterPayload,
     frenchflairPayload,
 
-    portfolio: getPortfolioSummary(),
-    analyses: getAnalyses(),
-    bets: getAllBets()
+    dashboard: getDashboardData(),
+
+    analyses: getAnalyses()
   };
 }
