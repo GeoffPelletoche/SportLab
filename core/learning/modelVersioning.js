f4e4aca43bee241a750142a4a972cde3f2a350ea
GeoffPelletoche <geoffrey.pelletier@mailo.com>
2026-08-02 17:03:07 +0200

    Add files via upload

--- core/learning/modelVersioning.js
+++ core/learning/modelVersioning.js
@@ -1,2 +1,2 @@
 import { MODEL_VERSIONS } from "../scoring/scoringSchema.js";
-export function getModelVersion(moduleId) { return MODEL_VERSIONS[moduleId] || "SPORTLAB-9.0.0"; }
+export function getModelVersion(moduleId) { return MODEL_VERSIONS[moduleId] || "SPORTLAB-10.0.0"; }
